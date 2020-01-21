16ece2902f5d01e1b94e862a0899f5563f787d98
Evan Barnes <evan.barnes.97@gmail.com>
2020-01-21 02:53:31 -0500

    Switched the TriggerManager to a singleton.

--- race_logger/managers/TriggerManager.py
+++ race_logger/managers/TriggerManager.py
@@ -1,49 +1,68 @@
+import logging
+
 from awebus import Bus
 
 from race_logger.structures.GPSData import GPSData
 from race_logger.utils import SettingsUtils
 
 
-class TriggerManager:
+_event_bus = None
+_is_triggered = False
+_start_line_crosses = 0
 
-    def __init__(self, event_bus: Bus):
+_laps_remaining = SettingsUtils.get("race_mode_settings", "laps")
+_time_remaining = SettingsUtils.get("race_mode_settings", "minutes")
 
-        self._event_bus = event_bus
-        self._is_triggered = False
-        self._start_line_crosses = 0
+_trigger_speed = SettingsUtils.get("race_mode_settings", "speed_trigger")
+_trigger_on_speed = SettingsUtils.get("race_mode_settings", "triggers", "speed")
+_trigger_on_one_pass = SettingsUtils.get("race_mode_settings", "triggers", "one_start_line_pass")
+_trigger_on_two_pass = SettingsUtils.get("race_mode_settings", "triggers", "two_start_line_passes")
 
-        self._laps_remaining = SettingsUtils.get("race_mode_settings", "laps")
-        self._time_remaining = SettingsUtils.get("race_mode_settings", "minutes")
 
-        self._trigger_speed = SettingsUtils.get("race_mode_settings", "speed_trigger")
-        self._trigger_on_speed = SettingsUtils.get("race_mode_settings", "triggers", "speed")
-        self._trigger_on_one_pass = SettingsUtils.get("race_mode_settings", "triggers", "one_start_line_pass")
-        self._trigger_on_two_pass = SettingsUtils.get("race_mode_settings", "triggers", "two_start_line_passes")
+# TODO: Add stop trigger (Includes laps/time remaining messages).
 
-        self._enable_event_callbacks()
 
-    async def _lap_handler(self):
-        self._start_line_crosses += 1
-        if self._trigger_on_one_pass and self._start_line_crosses >= 1:
-            await self._trip_trigger()
-        elif self._trigger_on_two_pass and self._start_line_crosses >= 2:
-            await self._trip_trigger()
+async def init(event_bus: Bus):
 
-    async def _gps_handler(self, gps_data: GPSData):
-        if self._trigger_on_speed and gps_data.speed > self._trigger_speed:
-            await self._trip_trigger()
+    global _event_bus
 
-    async def _trip_trigger(self):
-        await self._event_bus.emitAsync("trigger_tripped")
-        self._disable_event_callbacks()
+    logging.debug("Initializing the trigger manager.")
 
-    async def _reset_trigger(self):
-        self._enable_event_callbacks()
+    _event_bus = event_bus
+    _enable_event_callbacks()
 
-    def _enable_event_callbacks(self):
-        self._event_bus.on("crossed_start_line", self._lap_handler)
-        self._event_bus.on("gps_data", self._gps_handler)
 
-    def _disable_event_callbacks(self):
-        self._event_bus.off("crossed_start_line", self._lap_handler)
-        self._event_bus.off("gps_data", self._gps_handler)
+async def _lap_handler():
+    global _start_line_crosses
+    _start_line_crosses += 1
+    if _trigger_on_one_pass and _start_line_crosses >= 1:
+        logging.debug("The one pass start trigger was tripped.")
+        await _trip_trigger()
+    elif _trigger_on_two_pass and _start_line_crosses >= 2:
+        logging.debug("The two pass start trigger was tripped.")
+        await _trip_trigger()
+
+
+async def _gps_handler(gps_data: GPSData):
+    if _trigger_on_speed and gps_data.speed > _trigger_speed:
+        logging.debug("The speed start trigger was tripped.")
+        await _trip_trigger()
+
+
+async def _trip_trigger():
+    await _event_bus.emitAsync("trigger_tripped")
+    _disable_event_callbacks()
+
+
+async def _reset_trigger():
+    _enable_event_callbacks()
+
+
+def _enable_event_callbacks():
+    _event_bus.on("crossed_start_line", _lap_handler)
+    _event_bus.on("gps_data", _gps_handler)
+
+
+def _disable_event_callbacks():
+    _event_bus.off("crossed_start_line", _lap_handler)
+    _event_bus.off("gps_data", _gps_handler)
